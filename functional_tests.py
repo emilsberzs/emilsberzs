2df1eb8bb5ebddc8bbf8bb5aef7886b0ba8c00fc
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2021-01-10 20:22:22 +0000

    First functional test to check if homepage opens as supposed

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class HomePageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_opens_homepage_when_opening_site(self):
+
+        # Someone notices post about Emils portfolio website and goes ahed to check it out
+        self.browser.get('http://localhost:8000')
+
+        # Upon opening site, page title and header indicates that ir indeed is Emils portfolio website
+        self.assertIn('Emils', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
