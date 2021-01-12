5b498ffbb91d584f38752a752be946331ea3d45e
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2021-01-12 19:08:57 +0000

    Added logo, working on CSS

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+
 from .models import Projects
-from django.views.generic import ListView, DetailView
 
 
 def home_page(request):
